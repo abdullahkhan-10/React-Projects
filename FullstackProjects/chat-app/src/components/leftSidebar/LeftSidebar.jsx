2194559f82927c2e22d52fa13413714b673cf5db
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-24 00:00:40 +0500

    Small change made to chat page left side component

--- FullstackProjects/chat-app/src/components/leftSidebar/LeftSidebar.jsx
+++ FullstackProjects/chat-app/src/components/leftSidebar/LeftSidebar.jsx
@@ -1,46 +1,42 @@
-import myAssets from "../../assets/assets"
-import "./leftSidebar.css"
+import myAssets from "../../assets/assets";
+import "./leftSidebar.css";
 
 const LeftSidebar = () => {
   return (
     <div className="left-side">
-        {/* Top */}
-        <div className="ls-top">
-            <div className="ls-nav">
-                <img src={myAssets.logo} className="nav-logo" />
+      {/* Top */}
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={myAssets.logo} className="nav-logo" />
 
-                <div className="menu">
-                    <img src={myAssets.menu_icon} />
-                </div>
-            </div>
-
-            <div className="ls-search">
-                <img src={myAssets.search_icon} alt="" />
-                <input type="text" placeholder="Search here..." />
-            </div>
+          <div className="menu">
+            <img src={myAssets.menu_icon} />
+          </div>
         </div>
 
-        {/* Friends list  */}
-        <div className="ls-list">
-            <div className="friends">
-                <img src={myAssets.profile_img}  />
-
-                <div>
-                    <p>Denial Smith</p>
-                    <span>Hello, How are you?</span>
-                </div>
-            </div>
-            <div className="friends">
-                <img src={myAssets.profile_img}  />
-
-                <div>
-                    <p>Denial Smith</p>
-                    <span>Hello, How are you?</span>
-                </div>
-            </div>
+        <div className="ls-search">
+          <img src={myAssets.search_icon} alt="" />
+          <input type="text" placeholder="Search here..." />
         </div>
+      </div>
+
+      {/* Friends list  */}
+      <div className="ls-list">
+        {
+            Array(10).fill("").map((item, index) => (
+                <div key={index} className="friends">
+                    <img src={myAssets.profile_img} />
+
+                    <div>
+                        <p>George Smith</p>
+                        <span>Hello, How are you?</span>
+                    </div>
+                </div>
+            ))
+        }
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
