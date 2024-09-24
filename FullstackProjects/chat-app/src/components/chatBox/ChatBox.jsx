95cb56b48c85faaafcd1f694ab16b39895fd38ab
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-24 23:55:47 +0500

    chatBox UI feature added to chat page

--- FullstackProjects/chat-app/src/components/chatBox/ChatBox.jsx
+++ FullstackProjects/chat-app/src/components/chatBox/ChatBox.jsx
@@ -1,7 +1,49 @@
+import "./chatBox.css"
+import myAssets from "../../assets/assets"
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className="chat-box">
+      {/* user  */}
+      <div className="chat-user">
+        <img src={myAssets.profile_img}  />
+        <p>George Smith <img src={myAssets.green_dot} className="dot" /></p>
+        <img src={myAssets.help_icon} alt="" />
+      </div>
+
+      {/* users Messages */}
+      <div className="user-msg">
+        {/* sender message  */}
+        <div className="sender-msg">
+          <p className='msg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, vitae.</p>
+          <div>
+            <img src={myAssets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+
+        {/* Reciever message  */}
+        <div className="reciever-msg">
+          <p className='msg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, vitae.</p>
+          <div>
+            <img src={myAssets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+      {/* chat input  */}
+      <div className="chat-input">
+        <input type="text" placeholder='send a message' />
+
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor="image">
+          <img src={myAssets.gallery_icon}  />
+        </label>
+
+        <img src={myAssets.send_button} alt="" />
+      </div>
+    </div>
   )
 }
 
