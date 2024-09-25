cb3367778184b3d8e0e32017eb0d507911629305
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-25 23:18:03 +0500

    Right sidebar UI feature added

--- FullstackProjects/chat-app/src/components/rightSidebar/RightSidebar.jsx
+++ FullstackProjects/chat-app/src/components/rightSidebar/RightSidebar.jsx
@@ -1,7 +1,33 @@
+import myAssets from "../../assets/assets"
+import "./rightSidebar.css"
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <div className='right-side'>
+
+      {/* Profile */}
+      <div className="rs-profile">
+        <img src={myAssets.profile_img}  />
+        <h3>George Smith <img src={myAssets.green_dot} className='dot' /></h3>
+        <p>Hey, There I am George Smithusing chat app</p>
+      </div>
+      <hr />
+
+      {/* Media */}
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={myAssets.pic1} alt="" />
+          <img src={myAssets.pic2} alt="" />
+          <img src={myAssets.pic3} alt="" />
+          <img src={myAssets.pic4} alt="" />
+          <img src={myAssets.pic1} alt="" />
+          <img src={myAssets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+
+    </div>
   )
 }
 
