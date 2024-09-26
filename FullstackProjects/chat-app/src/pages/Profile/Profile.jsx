2dbf189311e669392f8a430898510e8c88c88978
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-26 23:30:05 +0500

    Profile page UI feature added

--- FullstackProjects/chat-app/src/pages/Profile/Profile.jsx
+++ FullstackProjects/chat-app/src/pages/Profile/Profile.jsx
@@ -1,8 +1,33 @@
 import "./profile.css"
+import myAssets from "../../assets/assets"
+import {useState} from "react"
 
 const Profile = () => {
+  const [image, setImage] = useState(false)
+  
+
+
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <div className="profile-container">
+        {/* form  */}
+        <form >
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input onChange={ (e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
+            <img src={image? URL.createObjectURL(image) : myAssets.avatar_icon} />
+            upload profile image
+          </label>
+
+          <input type="text" placeholder='Your name' required />
+          <textarea placeholder='write profile bio' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+
+        {/* right image  */}
+        <img src={image? URL.createObjectURL(image): myAssets.logo_icon} className='profile-pic' />
+      </div>
+    </div>
   )
 }
 
