8bc4b1a9d565570a29a5a32b385996515aacc56e
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-26 23:56:52 +0500

    Firebase setup added

---
+++ FullstackProjects/chat-app/src/config/configuration.js
@@ -0,0 +1,33 @@
+import { app } from "./firebase";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
+import { getFirestore, setDoc, doc } from "firebase/firestore"
+
+const auth = getAuth(app)
+const db = getFirestore()
+
+const signUp = async(userName, email, password) =>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const myUser = res.user
+
+        // Users info
+        // also add the info to database 
+        await setDoc( doc(db, "Users", myUser.userId), {
+            id: myUser.userId,
+            username: userName.toLowerCase(),
+            email,
+            name: "",
+            avatar: "",
+            bio: "",
+            lastSeen: Date.now()
+        })
+
+        // Chat info 
+        await setDoc(doc(db, "Chats", myUser.userId), {
+            chatData: []
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
+}
---
+++ FullstackProjects/chat-app/src/config/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH9ZgiLgOmz6veOfJvLXBXPf8lyg0jLNg",
+  authDomain: "chat-application-a2e67.firebaseapp.com",
+  projectId: "chat-application-a2e67",
+  storageBucket: "chat-application-a2e67.appspot.com",
+  messagingSenderId: "61796072916",
+  appId: "1:61796072916:web:50b58e88efb2ed53262a41"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
