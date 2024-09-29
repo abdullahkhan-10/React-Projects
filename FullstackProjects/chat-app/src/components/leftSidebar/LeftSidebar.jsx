261c834d30d139dd36da757b67435d7d94cb937e
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-09-29 23:42:58 +0500

    adding user chat to array and user search filter feature added

--- FullstackProjects/chat-app/src/components/leftSidebar/LeftSidebar.jsx
+++ FullstackProjects/chat-app/src/components/leftSidebar/LeftSidebar.jsx
@@ -2,8 +2,43 @@ import { useNavigate } from "react-router-dom";
 import myAssets from "../../assets/assets";
 import "./leftSidebar.css";
 
+import {collection, getDocs, query, where} from "firebase/firestore"
+import {db} from "../../config/configuration"
+import {AppContext} from "../../context/AppContext"
+import { useContext, useState } from "react";
+
 const LeftSidebar = () => {
   const navigate = useNavigate()
+  const {userData} = useContext(AppContext)
+
+  const [user, setUser] = useState(null)
+  const [showSearch, setShowSearch] = useState(false)
+
+
+  // To search our required user 
+  const searchUser = async(e)=>{
+    try {
+      const input = e.target.value
+      if (input) {
+        setShowSearch(true)
+        const userRef = collection(db, "Users")
+        const q = query(userRef, where("username", "==", input.toLowerCase()))
+        const querySnap = await getDocs(q)
+        // console.log(querySnap);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+
+          setUser(querySnap.docs[0].data())
+        }else{
+          setUser(null)
+        }
+
+      }else{
+        setShowSearch(false)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="left-side">
       {/* Top */}
@@ -24,23 +59,27 @@ const LeftSidebar = () => {
 
         <div className="ls-search">
           <img src={myAssets.search_icon} alt="" />
-          <input type="text" placeholder="Search here..." />
+          <input onChange={searchUser} type="text" placeholder="Search here..." />
         </div>
       </div>
 
       {/* Friends list  */}
       <div className="ls-list">
-        {
-            Array(10).fill("").map((item, index) => (
-                <div key={index} className="friends">
-                    <img src={myAssets.profile_img} />
+        {showSearch && user 
+          ? <div className="friends add-user">
+              <img src={user.avatar} />
+              <p>{user.name}</p>
+            </div>
+          : Array(10).fill("").map((item, index) => (
+            <div key={index} className="friends">
+                <img src={myAssets.profile_img} />
 
-                    <div>
-                        <p>George Smith</p>
-                        <span>Hello, How are you?</span>
-                    </div>
+                <div>
+                    <p>George Smith</p>
+                    <span>Hello, How are you?</span>
                 </div>
-            ))
+            </div>
+          ))
         }
       </div>
     </div>
--- FullstackProjects/chat-app/src/context/AppContext.jsx
+++ FullstackProjects/chat-app/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
-import { getDoc, doc, updateDoc } from 'firebase/firestore'
-import {createContext, useState} from 'react'
+import { getDoc, doc, updateDoc, onSnapshot } from 'firebase/firestore'
+import {createContext, useEffect, useState} from 'react'
 import {db} from "../config/configuration"
 import { useNavigate } from 'react-router-dom'
 import {auth} from "../config/configuration"
@@ -39,9 +39,32 @@ const ContextProvider = (props) =>{
                 })
             }
         }, 60000);
-        
-        
     }
+
+    // to store chats data in array 
+    useEffect( () =>{
+        if (userData) {
+            const chatRef = doc(db, "Chats", userData.id)
+            const allChats = onSnapshot(chatRef, async(res) =>{
+                const chatItems = res.data().chatData
+                // console.log(chatItems);
+                console.log(res.data());
+                const newData = []
+                for (const item of chatItems) {
+                    const userRef = doc(db, "Users", item.id)
+                    const userSnap = getDoc(userRef)
+                    const userData = userSnap.data()
+
+                    newData.push({...item, userData})
+                }
+                setChatData(newData.sort( (a,b) => b.updatedAt - a.updatedAt))
+                // console.log(chatData);
+            })
+            return () =>{
+                allChats();
+            }
+        }
+    },[userData])
 
     // data inside this object will be accessible to entire app
     const value = {
