ccd4f135f216fda921a554aa59ad1e68a74ae23d
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-10-08 23:42:26 +0500

    sidebar UI design feature added

--- FullstackProjects/geminiClone/src/components/sidebar/Sidebar.jsx
+++ FullstackProjects/geminiClone/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,42 @@
+import "./sidebar.css"
+import  {myAssets} from "../../assets/assets"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+        {/* top  */}
+        <div className="top">
+            <img src={myAssets.menu_icon} className='menu' />
+            <div className="new-chat">
+                <img src={myAssets.plus_icon}  />
+                <p>New Chat</p>
+            </div>
+
+            <div className="recent">
+                <p className='recent-list'>Recent</p>
+                <div className="recent-entry">
+                    <img src={myAssets.message_icon}  />
+                    <p>What is React...</p>
+                </div>
+            </div>
+        </div>
+        {/* bottom  */}
+
+        <div className="bottom">
+            <div className="bottom-item recent-entry">
+                <img src={myAssets.question_icon} alt="" />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={myAssets.history_icon} alt="" />
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={myAssets.setting_icon} alt="" />
+                <p>Setting</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
