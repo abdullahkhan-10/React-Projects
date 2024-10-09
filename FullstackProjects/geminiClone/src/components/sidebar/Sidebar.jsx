13dfa095f776735cee5bde1ed1f333aec0acc5cc
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-10-09 22:13:24 +0500

    design right side of app

--- FullstackProjects/geminiClone/src/components/sidebar/Sidebar.jsx
+++ FullstackProjects/geminiClone/src/components/sidebar/Sidebar.jsx
@@ -1,39 +1,46 @@
 import "./sidebar.css"
 import  {myAssets} from "../../assets/assets"
+import {useState} from "react"
 
 const Sidebar = () => {
+
+    const [extend, setExtend] = useState(true)
   return (
     <div className='sidebar'>
         {/* top  */}
         <div className="top">
-            <img src={myAssets.menu_icon} className='menu' />
+            <img onClick={ ()=>setExtend(prev => !prev)} src={myAssets.menu_icon} className='menu' />
             <div className="new-chat">
                 <img src={myAssets.plus_icon}  />
-                <p>New Chat</p>
+                {extend ? <p>New Chat</p>: null}
             </div>
 
-            <div className="recent">
-                <p className='recent-list'>Recent</p>
-                <div className="recent-entry">
-                    <img src={myAssets.message_icon}  />
-                    <p>What is React...</p>
+            {extend
+            ? <div className="recent">
+                  <p className='recent-title'>Recent</p>
+                  <div className="recent-entry">
+                     <img src={myAssets.message_icon}  />
+                     <p>What is React...</p>
+                  </div>
                 </div>
-            </div>
+            : null
+            }
+            
         </div>
         {/* bottom  */}
 
         <div className="bottom">
             <div className="bottom-item recent-entry">
                 <img src={myAssets.question_icon} alt="" />
-                <p>Help</p>
+                {extend? <p>Help</p>: null}
             </div>
             <div className="bottom-item recent-entry">
                 <img src={myAssets.history_icon} alt="" />
-                <p>Activity</p>
+                {extend? <p>Activity</p>: null}
             </div>
             <div className="bottom-item recent-entry">
                 <img src={myAssets.setting_icon} alt="" />
-                <p>Setting</p>
+                {extend? <p>Settings</p>: null}
             </div>
         </div>
     </div>
