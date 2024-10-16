f0d4c7563a38a9737aa7f8d71a31a2f58fad4323
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-10-16 23:25:00 +0500

    mobile responsive  design added

--- FullstackProjects/Saas-UI/src/Sections/Header.jsx
+++ FullstackProjects/Saas-UI/src/Sections/Header.jsx
@@ -1,46 +1,58 @@
+import { useState } from "react"
 import { Link as LinkScroll } from "react-scroll"
 
 // we will destractured the title and will recieve here as props.
 const NavLink = ({ title}) =>(
-    <LinkScroll>
-    {/* Will render the title here */}
+    
+    //  Will render the title here 
+    <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
       {title}
     </LinkScroll>
 )
+
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
   return (
     <header className="fixed top-0 left-0 z-50 w-full border border-red-400 py-10">
         <div className="border border-blue-400 container flex items-center h-14 max-lg:px-5">
+
             <a className="lg:hidden flex-1 cursor-pointer z-2"> 
                 <img src="/images/xora.svg" width={115} height={55} alt="log" />
             </a>
 
-            <div className="w-full border-2 border-amber-400">
-                <nav>
-                    <ul className="flex max-lg:block max-lg:px-12">
-                        {/* left side  */}
-                        <li className="nav-li">
-                            <NavLink title="features"/>
-                            <div className="dot"/>
-                            <NavLink title="pricing"/>
-                        </li>
+            {/* toggle menu  */}
+            <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+                <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
 
-                        {/* Midlle */}
-                        <li className="nav-logo">
-                            <LinkScroll>
-                                <img src="/images/xora.svg" width={160} height={55} alt="logo" />
-                            </LinkScroll>
-                        </li>
+                    <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+                        <ul className="flex max-lg:block max-lg:px-12">
+                            {/* left side  */}
+                            <li className="nav-li">
+                                <NavLink title="features"/>
+                                <div className="dot"/>
+                                <NavLink title="pricing"/>
+                            </li>
 
-                        {/* right side */}
-                        <li className="nav-li">
-                            <NavLink title="faq"/>
-                            <div className="dot"/>
-                            <NavLink title="download"/>
-                        </li>
-                    </ul>
-                </nav>
+                            {/* Midlle */}
+                            <li className="nav-logo">
+                                <LinkScroll>
+                                    <img src="/images/xora.svg" width={160} height={55} alt="logo" />
+                                </LinkScroll>
+                            </li>
+
+                            {/* right side */}
+                            <li className="nav-li">
+                                <NavLink title="faq"/>
+                                <div className="dot"/>
+                                <NavLink title="download"/>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
+
+            <button onClick={ () => setIsOpen(preState =>!preState)} className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"> <img src={`/images/${isOpen ? "close" : "magic"}.svg`} className="size-1/2 object-contain"/> </button>
+
         </div>
     </header>
   )
