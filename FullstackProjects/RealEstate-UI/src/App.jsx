3c8c0cba00e1c06e5f5ddcaf244d694b1a1cd508
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-11-13 23:30:16 +0500

    Projects section added

--- FullstackProjects/RealEstate-UI/src/App.jsx
+++ FullstackProjects/RealEstate-UI/src/App.jsx
@@ -1,11 +1,17 @@
 import About from './components/About'
 import Header from './components/Header'
+import Projects from './components/Projects'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
-      <Header/>
-      <About/>
+      <header>
+        <Header/>
+      </header>
+      <main>
+        <About/>
+        <Projects/>
+      </main>
     </div>
   )
 }
---
+++ FullstackProjects/RealEstate-UI/src/components/Projects.jsx
@@ -0,0 +1,13 @@
+
+const Projects = () => {
+  return (
+    <section className=" border border-red-600 container mx-auto py-4 pt-10 px-6 md:px-20 lg:px-32 my-10 w-full overflow-hidden" id="Projects">
+        <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Projects <span className="underline underline-offset-4 decoration-2 under font-light">Completed</span></h1>
+        <p className="text-center mx-auto text-gray-500 max-w-80 mb-6">Crafting Spaces, Building Legacies, Explore Our portofolio</p>
+
+        {/* Slider button  */}
+    </section>
+  )
+}
+
+export default Projects
