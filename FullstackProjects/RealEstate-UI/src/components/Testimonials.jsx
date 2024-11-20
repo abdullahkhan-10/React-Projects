1fecbcf835455939d1b91df1701d80021ed2158f
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-11-20 23:48:01 +0500

    Small change made in Testimonials section

--- FullstackProjects/RealEstate-UI/src/components/Testimonials.jsx
+++ FullstackProjects/RealEstate-UI/src/components/Testimonials.jsx
@@ -3,7 +3,7 @@ const Testimonials = () => {
   return (
     <section className="container mx-auto py-10 px-4 lg:px-32 w-full overflow-hidden" id="Testimonials">
         <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Customer <span className="underline underline-offset-4 decoration-2 under font-light">Testimonials</span></h1>
-        <p className="text-center">Real Stories from those who found Home with us</p>
+        <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">Real Stories from those who found Home with us</p>
 
         <div className="flex flex-wrap justify-center gap-8 mt-4 ">
             {
