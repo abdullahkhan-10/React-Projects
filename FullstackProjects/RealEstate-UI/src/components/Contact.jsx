e42a9aa3a64c8c4d44db3a7dd3b7ae077fe0a1a3
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-11-23 00:18:02 +0500

    Functionality added to contact form

--- FullstackProjects/RealEstate-UI/src/components/Contact.jsx
+++ FullstackProjects/RealEstate-UI/src/components/Contact.jsx
@@ -1,6 +1,35 @@
 import { motion } from "motion/react"
+import { useState } from "react";
 
 const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", " df199d8d-0876-4f65-9370-06a4ca2faa5d"); 
+
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("");
+        alert("Form Submitted successfully")
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult("");
+        alert(data.message)
+      }
+    };
   return (
     <motion.div
         initial={{opacity:0, x: -200}}
@@ -11,7 +40,7 @@ const Contact = () => {
         <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Contact <span className="underline underline-offset-4 decoration-2 under font-light">With Us</span></h1>
         <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">Ready to make a Move? Let's Build your future Together</p>
 
-        <form className="max-w-3xl mx-auto text-gray-600 pt-6" >
+        <form onSubmit={onSubmit} className="max-w-3xl mx-auto text-gray-600 pt-6" >
             {/* input box */}
             <div className="flex flex-wrap  p-4">
 
@@ -33,7 +62,7 @@ const Contact = () => {
             </div>
 
             {/* button */}
-            <button className="bg-blue-500 text-white py-2 px-10 rounded">Send Message</button>
+            <button className="bg-blue-500 text-white py-2 px-10 rounded">{ result ? result : "Send Message"}</button>
         </form>
     </motion.div>
   )
