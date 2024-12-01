f52c811a756dfdc9b98e0835e9932f333383316c
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-12-01 12:03:51 +0500

    Albums data added to Home

--- FullstackProjects/spotify-clone/src/components/Home.jsx
+++ FullstackProjects/spotify-clone/src/components/Home.jsx
@@ -1,10 +1,27 @@
 import Navbar from "./Navbar"
+import { albumsData } from "../assets/assets"
+import Albums from "./Albums"
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
-    </div>
+    <>
+      <Navbar/>
+
+      {/* Albums data  */}
+      <div className="mb-4">
+          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+          <div className="flex overflow-auto">
+            {
+                albumsData.map( (item, index) =>(
+                    <Albums key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+                ))
+            }
+          </div>
+      </div>
+
+      {/* songs data  */}
+      
+    </>
   )
 }
 
