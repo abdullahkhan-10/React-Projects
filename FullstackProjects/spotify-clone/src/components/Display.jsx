baa58bf9aac5d4f8ef0f3c9adbe65aa34f6ef855
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-12-02 23:21:54 +0500

    Background color feature added to Display component

--- FullstackProjects/spotify-clone/src/components/Display.jsx
+++ FullstackProjects/spotify-clone/src/components/Display.jsx
@@ -1,10 +1,40 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, useLocation} from "react-router-dom"
 import Home from "./Home"
 import DisplayAlbum from "./DisplayAlbum"
+import { useEffect, useRef } from "react"
+import { albumsData } from "../assets/assets"
 
 const Display = () => {
+
+  // to display background color 
+
+  const displayRef = useRef()
+  const location = useLocation()
+  // console.log(location);
+
+  const isAlbum = location.pathname.includes("album")
+  // console.log(isAlbum);
+
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";  
+  // console.log(albumId);
+
+  const backgroundColor = albumsData[Number(albumId)].bgColor
+  // console.log(backgroundColor);
+
+  useEffect( () =>{
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient( ${backgroundColor}, #121212)`
+
+    }else{
+      displayRef.current.style.background = `#121212`
+    }
+  })
+  
+  
+  
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:ml-0">
+    // we will change the background color of (Display.jsx) component when click on any (album). 
+    <div ref={displayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:ml-0">
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/album/:id" element={<DisplayAlbum/>} />
