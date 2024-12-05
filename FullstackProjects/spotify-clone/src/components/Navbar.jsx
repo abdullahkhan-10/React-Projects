59930f50de54c0403df6560b7f1e03e636b3e688
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-12-05 23:01:06 +0500

    Change made to Navbar component

--- FullstackProjects/spotify-clone/src/components/Navbar.jsx
+++ FullstackProjects/spotify-clone/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
   
   return (
     <>
+      {/* Left side */}
       <div className="w-full flex justify-between items-center font-semibold">
             <div className="flex items-center gap-2">
                 <img onClick={ () => navigate(-1)} src={assets.arrow_left} className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
@@ -20,6 +21,7 @@ const Navbar = () => {
             </div>
       </div>
 
+      {/* Right side */}
       <div className="flex items-center gap-2 mt-4">
         <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">All</p>
         <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
